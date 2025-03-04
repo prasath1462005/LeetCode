c72fc2627d67187cdb7e25255204ac5987f893a1
s-prasath <prasathsuresh2406@gmail.com>
2025-03-04 15:56:43 +0530

    Time: 12 ms (23.03%), Space: 45 MB (66.42%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[][] vis;
+    int[][] dp;
+    int[][] dir=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+    public int longestIncreasingPath(int[][] m) {
+        vis=new int[m.length][m[0].length];
+        dp=new int[m.length][m[0].length];
+        int max=0;
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[0].length;j++){
+                int v=dfs(i,j,m);
+                max=Math.max(max,v);
+                if(dp[i][j]<v)dp[i][j]=v;
+            }
+        }
+        /*for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }*/
+        return max;
+    }
+    public int dfs(int r,int c,int[][] m){
+        int in=0;
+        if(dp[r][c]!=0){
+            return dp[r][c];
+        }
+        vis[r][c]=1;
+        int max=0;
+        for(int[] n:dir){
+            int nr=n[0]+r;
+            int nc=n[1]+c;
+            if(nr>=0 && nr<m.length && nc>=0 && nc<m[0].length && vis[nr][nc]!=1 && m[r][c]<m[nr][nc]){
+                in=1;
+                int v=1+dfs(nr,nc,m);
+                max=Math.max(max,v);
+                dp[r][c]=max;
+                
+            }
+        }
+        vis[r][c]=0;
+        if(in==1)return max;
+        return 1;
+    }
+}
