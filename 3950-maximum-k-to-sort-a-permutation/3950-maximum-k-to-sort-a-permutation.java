8f4c9f334d0d916eefced3e9c6db70fec10eaf35
s-prasath <prasathsuresh2406@gmail.com>
2025-08-10 13:21:48 +0530

    Time: 398 ms (5.07%), Space: 71.7 MB (5.14%) - LeetHub

---
+++ 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int sortPermutation(int[] nums) {
+        int max=0;
+        int[] v=new int[nums.length];
+        List<Integer> l=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i==nums[i])continue;
+            if(v[i]!=1){
+                v[i]=1;
+                l.add(dfs(nums,nums[i],i,v));
+            }
+        }
+        Collections.sort(l);
+        System.out.println(l);
+        if(l.isEmpty())return 0;
+        return l.get(0);
+    }
+    public int dfs(int[] nums,int i,int ans,int[] v){
+        System.out.println(ans);
+        if(v[i]==1)return ans;
+        v[i]=1;
+        return dfs(nums,nums[i],ans&i,v);
+    }
+}
