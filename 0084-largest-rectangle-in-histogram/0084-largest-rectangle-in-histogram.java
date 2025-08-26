3e87087f29395325d87284817f143b1ca04626dd
s-prasath <prasathsuresh2406@gmail.com>
2025-08-26 14:52:38 +0530

    Time: 64 ms (76.28%), Space: 57.6 MB (44.28%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,36 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] nse=new int[heights.length];
-        int[] pse=new int[heights.length];
-        fnse(nse,heights);
-        fpse(pse,heights);
+        Stack<Integer> s=new Stack<>();
         int max=0;
-        for(int i=0;i<nse.length;i++){
-            max=Math.max(max,heights[i]*(nse[i]-pse[i]-1));
+        for(int i=0;i<heights.length;i++){
+            while(!s.isEmpty() && heights[i]<heights[s.peek()]){
+                int right=i;
+                int curr=s.pop();
+                int left=s.isEmpty()?-1:s.peek();
+                //System.out.println(left+" "+right);
+                max=Math.max(max,heights[curr]*(i-left-1));
+                //System.out.println(max);
+            }
+            s.push(i);
+            //System.out.println(s);
+        }
+        while(!s.isEmpty()){
+            int curr = s.pop();
+            int left = s.isEmpty() ? -1 : s.peek();
+            max = Math.max(max, heights[curr] * (heights.length - left - 1));
         }
         return max;
-    }
-    public void fnse(int[] arr,int[] h){
-        int i=h.length-1;
-        Stack<Integer> s=new Stack<>();
-        for(;i>=0;i--){
-            while(!s.isEmpty() && h[s.peek()]>=h[i]){
-                s.pop();
-            }
-            arr[i]=s.isEmpty()?h.length:s.peek();
-            s.push(i);
-            System.out.println(s);
-        }
-    }
-    public void fpse(int[] arr,int[] h){
-        int i=0;
-        Stack<Integer> s=new Stack<>();
-        for(;i<h.length;i++){
-            while(!s.isEmpty() && h[s.peek()]>=h[i]){
-                s.pop();
-            }
-            arr[i]=s.isEmpty()?-1:s.peek();
-            s.push(i);
-        }
     }
 }
